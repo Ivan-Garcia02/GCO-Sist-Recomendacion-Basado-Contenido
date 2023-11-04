92a9e9e2b94fbfd7471d56d81cc67246192f7a53
Ivan <ivitacoronte@hotmail.com>
2023-11-04 13:56:18 +0000

    matriz termino-documento obtenida

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import argparse
 import json
+import numpy as np
 from file_reader import read_documents_file, read_stop_words_file, read_lemmatization_file
+# python3 src/main.py -d test_files/documents-01.txt -s test_files/stop-words-en.txt -l test_files/corpus-en.txt
 
 def documents_lemmatization(documents, lemmatization):
     for i in range(len(documents)):
@@ -37,3 +39,34 @@ lemmatization = read_lemmatization_file(args.lemmatizationFilename)
 print(documents)
 documents_lemmatization(documents, lemmatization)
 remove_stop_words(documents, stop_words)
+print(documents)
+
+values = []
+for document in documents[:]: # Obtener las columnas con las palabras sin repetir en values
+    for word in document[:]:
+        try:
+            values.index(word)
+        except: 
+            values.append(word)
+print(values)
+
+matrix = np.zeros((len(documents), len(values))) # Creamos la matriz con 0s
+for idex_doc in range(len(documents)): # Hacemos el conteo para la matriz termino documento
+    for word in documents[idex_doc]:
+        index_word = values.index(word)
+        matrix[idex_doc][index_word] += 1
+
+
+# Imprimimos la matrix en un archivo
+with open('salida.txt', mode='w') as file_object:
+    max_string_length = max (len(word) for word in values) # Obtener la palabra mas larga para max de columna
+    for word in values:
+        file_object.write("{:<{width}} ".format(word, width=max_string_length))
+    file_object.write('\n')
+
+    for fila in matrix:
+        for col in fila:
+            file_object.write("{:<{width}} ".format(col, width=max_string_length))
+        file_object.write('\n')
+
+print(matrix)
